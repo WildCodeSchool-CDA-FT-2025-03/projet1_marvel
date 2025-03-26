fd1adc7a02f31c4323ebf09ac90a786d9762986a
Dilgo-dev <dilgopierre@gmail.com>
2025-03-26 09:52:06 +0100

    feat(server): Add basic apollo server

---
+++ server/src/index.ts
@@ -0,0 +1,29 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `
+    type Query {
+        hello: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => "Hello, world!",
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+async function startServer() {
+    const { url } = await startStandaloneServer(server, {
+        listen: { port: 4000 },
+    });
+
+    console.log(`🚀 Server ready at ${url}`);
+}
+
+startServer();
