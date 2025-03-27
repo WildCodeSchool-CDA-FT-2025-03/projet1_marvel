51de97b370bfc588a5580d4a1f178f6b749b4b41
Dilgo-dev <dilgopierre@gmail.com>
2025-03-27 16:04:11 +0100

    feat(client): Add Client pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
 import Layout from './components/layout/Layout';
+import CataloguePage from './pages/Catalogue';
+import FavoritePage from './pages/Favorite';
+import ItemPage from './pages/Item';
 
 const router = createBrowserRouter([
   {
@@ -10,6 +13,18 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <HomePage />,
+      },
+      {
+        path: '/catalogue',
+        element: <CataloguePage />,
+      },
+      {
+        path: '/favorite',
+        element: <FavoritePage />,
+      },
+      {
+        path: '/item/:id',
+        element: <ItemPage />,
       },
     ],
   },
