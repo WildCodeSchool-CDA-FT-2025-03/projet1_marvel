c2b6616858d2897b537661e4be43197503c6589e
Dilgo-dev <dilgopierre@gmail.com>
2025-04-02 16:18:25 +0200

    fix(server): Extend class Game with BaseEntity from typeorm

--- server/src/entities/game.entity.ts
+++ server/src/entities/game.entity.ts
@@ -1,7 +1,7 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
 
 @Entity('game')
-export class Game {
+export class Game extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
