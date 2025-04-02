01b86fb16c4301db9fa7cf41a1839ea540b776cc
Dilgo-dev <dilgopierre@gmail.com>
2025-04-02 16:17:47 +0200

    feat(server): Import Game entity in data source

--- server/src/database/client.ts
+++ server/src/database/client.ts
@@ -1,11 +1,12 @@
 import { DataSource } from 'typeorm';
 
 import { Hello } from '../entities/hello.entity';
+import { Game } from '../entities/game.entity';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: process.env.DATABASE_URL || './db.sqlite',
-  entities: [Hello],
+  entities: [Hello, Game],
   synchronize: true,
   logging: true,
 });
