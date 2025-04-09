94f93181a413276d6667c255209bd5aced2e1873
Flam64 <celio.mozes.rocha@gmail.com>
2025-04-09 11:56:47 +0200

    feat(server): ajout resolver getOneMusicById

--- server/src/resolvers/music.resolver.ts
+++ server/src/resolvers/music.resolver.ts
@@ -1,10 +1,21 @@
-import { Resolver, Query } from 'type-graphql';
+import { Resolver, Query, Arg } from 'type-graphql';
 import { Music } from '../entities/music.entity';
 
 @Resolver(Music)
 export class MusicResolver {
+  // return all musics and tracks
   @Query(() => [Music])
   async getMusic(): Promise<Music[]> {
     return await Music.find({ relations: { tracklist: true } });
+  }
+
+  // return one music and tracks by id
+  // I use findOne instead of findOneById because I need to use "relations" with the "where" clause
+  @Query(() => Music)
+  async getOneMusicById(@Arg('id') id: string): Promise<Music> {
+    return (await Music.findOne({
+      where: { id: +id },
+      relations: { tracklist: true },
+    })) as Music;
   }
 }
