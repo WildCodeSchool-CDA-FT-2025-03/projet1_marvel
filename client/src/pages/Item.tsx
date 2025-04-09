3a9659dfa250fb6c9c5ecd265d6974eb296ced69
rodolphe <rodolphe.tournier@gmail.com>
2025-04-09 16:32:01 +0200

    fix: clean file item

--- client/src/pages/Item.tsx
+++ client/src/pages/Item.tsx
@@ -1,17 +1,3 @@
-import DetailComponent from '../components/layout/detail';
-
 export default function ItemPage() {
-  // Ici vous pouvez récupérer les données de l'item depuis votre API
-  const itemData = {
-    itemTitle: "Titre de l'item",
-    // Autres données de l'item...
-  };
-
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <div className="max-w-4xl mx-auto">
-        <DetailComponent {...itemData} />
-      </div>
-    </div>
-  );
+  return <>ItemPage...</>;
 }
